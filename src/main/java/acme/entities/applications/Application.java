1c0d156bc7b489e3da6e65c4c1ffd9316807769c
migyanari-us <56026591+migyanari-us@users.noreply.github.com>
2019-11-24 17:19:37 +0100

    Creadas todas las entidades necesarias para el item C

---
+++ src/main/java/acme/entities/applications/Application.java
@@ -0,0 +1,62 @@
+
+package acme.entities.applications;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.entities.jobs.Job;
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Application extends DomainEntity {
+
+	private static final long	serialVersionUID	= 3L;
+
+	@Column(unique = true)
+	@NotBlank
+	@Length(min = 9, max = 14)
+	private String				reference;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				moment;
+
+	@NotBlank
+	private String				status;
+
+	@NotBlank
+	private String				statement;
+
+	@NotBlank
+	private String				skills;
+
+	@NotBlank
+	private String				qualifications;
+
+	// Relationship
+
+	@NotNull
+	@OneToOne(optional = false)
+	private Job					job;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Worker				worker;
+}
