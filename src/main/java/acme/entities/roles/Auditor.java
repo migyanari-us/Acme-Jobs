47e93b42ac46aa62727cd56be966569e42ea8de4
migyanari-us <56026591+migyanari-us@users.noreply.github.com>
2019-11-25 19:35:55 +0100

    Arreglando el Auditor de Quique.

--- src/main/java/acme/entities/roles/Auditor.java
+++ src/main/java/acme/entities/roles/Auditor.java
@@ -1,17 +1,23 @@
 
 package acme.entities.roles;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 
 import acme.framework.entities.UserRole;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Auditor extends UserRole {
 
-	private static final long	serialVersionUID	= 1L;
+	private static final long	serialVersionUID	= 18L;
 
 	@NotBlank
 	private String				firm;
 
 	@NotBlank
-	private Boolean				responsibilityStatement;
+	private String				responsibilityStatement;
 }
