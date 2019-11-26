5c58e6e9cdf5452c13b1143f800a25c96b3225ad
pedbiefre <44770266+pedbiefre@users.noreply.github.com>
2019-11-26 17:58:21 +0100

    Modelo, vistas y entidades creadas [Faltan repository y controller]

---
+++ src/main/java/acme/entities/auditRecord/AuditRecord.java
@@ -0,0 +1,52 @@
+
+package acme.entities.auditRecord;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import acme.entities.jobs.Job;
+import acme.entities.roles.Auditor;
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AuditRecord extends DomainEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	private String				title;
+
+	@NotBlank
+	private String				status;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				moment;
+
+	@NotBlank
+	private String				body;
+
+	//Relationship
+
+	@NotNull
+	@OneToOne(optional = false)
+	private Job					job;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Auditor				auditor;
+}
