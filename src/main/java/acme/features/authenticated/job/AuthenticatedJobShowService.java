2a6e9c11b13097eac4eb6ce24b2cfd3b2fa7d031
migyanari-us <56026591+migyanari-us@users.noreply.github.com>
2019-12-02 21:23:38 +0100

    Update AuthenticatedJobShowService.java

--- src/main/java/acme/features/authenticated/job/AuthenticatedJobShowService.java
+++ src/main/java/acme/features/authenticated/job/AuthenticatedJobShowService.java
@@ -29,8 +29,7 @@ public class AuthenticatedJobShowService implements AbstractShowService<Authenti
 		assert entity != null;
 		assert model != null;
 
-		request.unbind(entity, model, "reference", "title", "deadline");
-		request.unbind(entity, model, "salary", "moreInfo", "finalMode");
+		request.unbind(entity, model, "reference", "title", "deadline", "salary", "moreInfo", "finalMode");
 	}
 
 	@Override
